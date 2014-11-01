2f740ef80c90e7a20697cd795db8d6401d4660cc
oret <oretyoku@yahoo.co.jp>
2014-11-01 23:35:31 +0900

    Add gulp livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
 var gulp = require('gulp');
 var mainBowerFiles = require('main-bower-files');
+var nodemon = require('gulp-nodemon');
+var livereload = require('gulp-livereload');
+
+gulp.task('server', function () {
+  livereload.listen();
+
+  nodemon({
+    script: './bin/www', // express 4系の場合
+    // script: 'app.js', // express 3系の場合
+    ext: 'html js ejs css',
+    ignore: ['ignored.js', 'node_modules', 'bower_components'] })
+    .on('start', function () {
+      console.log('started!');
+      livereload.changed();  // Browserにlivereloadを通知
+    });
+});
 
 gulp.task('bower-files', function() {
     return gulp.src(mainBowerFiles(/* options */),
@@ -7,4 +23,4 @@ gulp.task('bower-files', function() {
                .pipe(gulp.dest('./public/javascripts'))
 });
 
-gulp.task('default', ['bower-files']);
+gulp.task('default', ['bower-files', 'server']);
